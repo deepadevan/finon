a6215da9e0b1bf1d62a18e9726ff38ba487b42dd
deepadevan <deepa@euphontec.com>
2014-09-11 18:19:25 +0530

    setup.php updated

--- contract/controllers/setup.php
+++ contract/controllers/setup.php
@@ -13,41 +13,28 @@ class Setup extends Admin_Controller {
 	function install() {
 
 		$queries = array(
-		
-		"CREATE TABLE IF NOT EXISTS `contract_content` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `contract_id` int(11) NOT NULL,
-		  `customer_id` int(11) NOT NULL,
-		  `serial_no` int(11) NOT NULL,
-		  `inventory_id` int(11) NOT NULL,
-		  `qty` int(11) NOT NULL,
-		  `unit_price` int(11) NOT NULL,
-		  `total_price` int(11) NOT NULL,
-		  PRIMARY KEY (`id`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-		
-		CREATE TABLE IF NOT EXISTS `contract_record` (
-		  `contract_id` int(11) NOT NULL AUTO_INCREMENT,
-		  `customer_id` int(11) NOT NULL,
-		  `start_date` date NOT NULL,
-		  `end_date` date NOT NULL,
-		  `billing_cycle` varchar(100) NOT NULL,
-		  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-		  PRIMARY KEY (`contract_id`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-		
-		CREATE TABLE IF NOT EXISTS `inventory` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `inventory_item` varchar(100) NOT NULL,
-		  PRIMARY KEY (`id`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+				   '0' => "CREATE TABLE IF NOT EXISTS `contract_content` (
+				  `id` int(11) NOT NULL AUTO_INCREMENT,
+				  `contract_id` int(11) NOT NULL,
+				  `customer_id` int(11) NOT NULL,
+				  `serial_no` int(11) NOT NULL,
+				  `inventory_id` int(11) NOT NULL,
+				  `qty` int(11) NOT NULL,
+				  `unit_price` int(11) NOT NULL,
+				  `total_price` int(11) NOT NULL,
+				  PRIMARY KEY (`id`)
+				) ENGINE=MyISAM DEFAULT CHARSET=utf8;",
+				'1'=>"CREATE TABLE IF NOT EXISTS `contract_record` (
+				  `contract_id` int(11) NOT NULL AUTO_INCREMENT,
+				  `customer_id` int(11) NOT NULL,
+				  `start_date` date NOT NULL,
+				  `end_date` date NOT NULL,
+				  `billing_cycle` varchar(100) NOT NULL,
+				  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+				  PRIMARY KEY (`contract_id`)
+				) ENGINE=MyISAM DEFAULT CHARSET=utf8;"
+				);
 
-		CREATE TABLE IF NOT EXISTS `users` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `name` varchar(100) NOT NULL,
-		  PRIMARY KEY (`id`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8;"		
-		);
 
 		foreach ($queries as $query) {
 
